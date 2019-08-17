15681efc412038fc5914ca5a923d2834414249f9
davood500 <37255133+davood500@users.noreply.github.com>
2019-08-17 10:22:52 -0700

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -2,14 +2,14 @@
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := rk2928sdk
+PRODUCT_RELEASE_NAME := rk30sdk
 
 # Inherit device configuration
-$(call inherit-product, device/rockchip/rk2928sdk/device_rk2928sdk.mk)
+$(call inherit-product, device/rockchip/rk30sdk/device_rk30sdk.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := rk2928sdk
-PRODUCT_NAME := cm_rk2928sdk
-PRODUCT_BRAND := EMERSON
-PRODUCT_MODEL := EM744
+PRODUCT_DEVICE := rk30sdk
+PRODUCT_NAME := cm_rk30sdk
+PRODUCT_BRAND := TECLAST
+PRODUCT_MODEL := P10
 PRODUCT_MANUFACTURER := rockchip
